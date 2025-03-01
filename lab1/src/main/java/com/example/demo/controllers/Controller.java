0297b1ad3d8a5e4a3794a5886c7e87f8c9889021
hassanxadel <89996749+hassanxadel@users.noreply.github.com>
2025-03-01 02:43:24 +0200

    init project

--- lab1/src/main/java/com/example/demo/controllers/Controller.java
+++ lab1/src/main/java/com/example/demo/controllers/Controller.java
@@ -1,6 +1,32 @@
 package com.example.demo.controllers;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+
+@GetMapping("/hello world")
+public String sayHello(){
+    return "Hello World";
+}
+
+@DeleteMapping("/hello delete")
+    public String sayHello2(){
+    return "Hello Delete";
+    }
+
+@PostMapping("/hello post")
+    public String sayHello3(){
+        return "Hello Post";
+    }
+    
+@PutMapping("/hello put")
+    public String sayHello4(){
+        return "Hello Put";
+    }
 }
